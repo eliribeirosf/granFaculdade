11340a55dd6c8a13a82517585da6f2aba9be5e5a
Elias <eliribeirosf@gmail.com>
2023-08-07 08:42:32 -0300

    new module

---
+++ Primeiro Período/Módulo 2/js/scripts-aula01.js
@@ -0,0 +1,15 @@
+// Trabalhando com estruturas condicionais (if, if-else e else)
+let media = 7;
+if (media >= 7){
+    console.log("Aprovado!");
+}
+else if(media>=5 && media<7){
+    console.log("Recuperação!");
+}
+else{
+    console.log("Reprovado!");
+}
+
+// Trabalhando com operador ternário
+let mediaTernario = 5;
+mediaTernario>=7 ? console.log("Aprovado!") : console.log("Reprovado!")
